bcb2aed50d10d8df95c41ba7df899e04a71a429a
sharlyf Syani <shirolyfsyani@gmail.com>
2023-11-22 20:37:37 +0700

    hai

---
+++ Bioskopwithscanners27.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Bioskopwithscanners27 {
+    public static void main(String[] args) {
+        
+        Scanner input08 = new Scanner(System.in);
+
+        
+        String penonton[][] = new String[4][2];
+
+        
+
+
+       
+        while (true) {
+        
+            System.out.println("Daftar Menu : ");
+            System.out.println("1. Menu input data penonton");
+            System.out.println("2. Menu tampilkan data penonton");
+            System.out.println("3. Menu exit");
+            System.out.print("Masukkan menu yang ingin dipilih(1,2,3) : ");
+            int pilihan = input08.nextInt();
+            System.out.println();
+
+           
+            switch (pilihan) {
+                case 1:
+                    
+                    System.out.print("Masukkan nama : ");
+                    String nama = input08.next();
+                    int baris, kolom;
+
+                    while (true) {
+                        System.out.print("masukkan baris : ");
+                        baris = input08.nextInt();
+                        System.out.print("masukkan kolom : ");
+                        kolom = input08.nextInt();
+
+                        if (baris > 0 && baris <= penonton.length && kolom > 0 && kolom <= penonton[0].length) {
+                           if (penonton[baris-1][kolom-1] == null) {
+                             penonton[baris-1][kolom-1] = nama;
+                             break;
+                           } else {
+                            System.out.println();
+                            System.out.println("Kursi yang anda inginkan sudah terisi! masukkan nomor kursi yang lain!!");
+                           }
+                        } else {
+                            System.out.println();
+                            System.out.println("Baris dan kolom anda tidak tersedia! Coba masukkan lagi");
+                        }
+                    }
+                break;
+                   
+
+                case 2:
+                
+                System.out.println("Daftar penonton");
+                for (int i = 0; i < penonton.length; i++) {
+                    
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.println("Penonton baris " + (i+1) + ", " + "kolom" + (j+1) + ": ***");
+                        } else {
+                            System.out.println("Penonton baris " + (i+1) + ", " + "kolom" + (j+1) + ": " + penonton[i][j]);
+                        }
+                    }
+                }
+                System.out.println();
+                break;
+                
+                case 3:
+                //exit program
+                System.out.println("Terimakasih kami ucapkan");
+                System.exit(0);
+               break;
+
+                default:
+                System.out.println("Pilihan menu anda tidak sesuai, mohon input menu lagi");
+                break;
+            }
+
+          
+                System.out.println();
+                System.out.println("kembali lagi ke daftar menu? (y/n) : ");
+                String next = input08.next();
+                System.out.println();
+            
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
